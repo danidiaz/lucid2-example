3c6fc2fcfc814bfd2e93a4624f823f755df72ebc
Daniel Díaz <diaz_carrete@yahoo.com>
2024-05-03 21:01:01 +0200

    ...

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 [Tailwind installation](https://tailwindcss.com/docs/installation)
 
 ```
+ormolu --mode inplace $(git ls-files '*.hs')
+
 ghcid -r
 
 npx tailwindcss -i ./input.css -o ./output.css --watch
--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,28 @@
-{-# LANGUAGE OverloadedStrings, ExtendedDefaultRules, BlockArguments #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Function ((&))
 import Lucid
+import Network.HTTP.Types (status200)
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
-import Network.HTTP.Types (status200)
-import Data.Function ((&))
 
 page :: Html ()
 page = do
-    html_ do
-        head_ do
-            title_ "foo"
-            link_ [rel_ "stylesheet", href_ "output.css"]
-            pure ()
-        body_ do
-            div_ [class_ "m-auto bg-emerald-300 w-3/4"] "foo"
+  html_ do
+    head_ do
+      title_ "foo"
+      link_ [rel_ "stylesheet", href_ "output.css"]
+      pure ()
+    body_ do
+      div_ [class_ "m-auto bg-emerald-300 w-3/4"] "foo"
 
 app :: Application
-app req f = 
-   case req & pathInfo of
+app req f =
+  case req & pathInfo of
     ["output.css"] -> f (responseFile status200 [] "./output.css" Nothing)
     _ -> f (responseLBS status200 [] (Lucid.renderBS page))
 
